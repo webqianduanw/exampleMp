eae2684b20d6c9daad354a047ca2a233ef93e19a
simplePoint <971977541@qq.com>
2019-11-07 14:51:57 +0800

    fix:代码提交

--- src/h5/index/index/App.vue
+++ src/h5/index/index/App.vue
@@ -1,37 +1,49 @@
 <template>
   <div class="example">
-
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <div class="refreshImg" slot="loading"><img src="@/assets/img/example/refresh.gif"/></div>
+      <div class="refreshImg" slot="loosing"><img src="@/assets/img/example/refresh.png"/></div>
+      <div class="refreshImg" slot="pulling"><img src="@/assets/img/example/refresh.png"/></div>
+      <div class="content">
+        我是主题内容
+      </div>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
 // import moduleName from 'module'
+import { PullRefresh } from 'vant';
 
 export default {
-  name: '',
-  components: {
+    name: '',
+    components: {
+        VanPullRefresh: PullRefresh
+    },
+    data() {
+        return {
+            isLoading: false
+        };
+    },
+    methods: {
+        onRefresh() {
+            window.location.reload();
+        }
+    },
+    created() {
 
-  },
-  data () {
-    return {
+    },
+    mounted() {
+
+    },
+    updated() {
 
     }
-  },
-  methods: {
-
-  },
-  created () {
-
-  },
-  mounted () {
-
-  },
-  updated () {
-
-  }
-}
+};
 </script>
 
 <style lang="scss">
-
+.example{
+  height: 100%;
+}
 </style>
